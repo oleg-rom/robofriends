5443806b0a98a2053530431d0e03bf4ca9427bbb
Oleg Romanenko <oleginga@gmail.com>
2024-09-11 17:53:36 +0200

    gramma fixed

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {  } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
